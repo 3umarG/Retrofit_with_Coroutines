02406155d551f051c746ac4825db7e6bbec41d0b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-10-13 17:45:14 +0200

    Add Circular Progress Bar Loading Feature Commit

--- app/src/main/java/com/example/simpleretrofitwithcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/simpleretrofitwithcoroutines/MainActivity.kt
@@ -2,17 +2,24 @@ package com.example.simpleretrofitwithcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.coroutines.*
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tv: TextView
+    private lateinit var pb: ProgressBar
+
     @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.tv)
+        pb = findViewById(R.id.pb)
 
 
         val api: MyApi = Retrofit.Builder()
@@ -23,10 +30,16 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch {
             val response: Response<List<Comment>> = api.getComments()
-            if (response.isSuccessful){
-                for (comment in response.body()!!){
-                    println("Comment : " +
-                            "$comment")
+            if (response.isSuccessful) {
+                withContext(Dispatchers.Main) {
+                    tv.visibility = View.VISIBLE
+                    pb.visibility = View.GONE
+                    for (comment in response.body()!!) {
+                        println(
+                            "Comment : " +
+                                    "$comment"
+                        )
+                    }
                 }
             }
         }
